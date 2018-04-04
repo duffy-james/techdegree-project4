4056eabb7b834edf9b398c61667f08949a224286
James Duffy <jduffy2000@gmail.com>
2018-04-04 16:17:07 -0400

    Updates to gallery
    Lined up the page to follow mockup. Added styling to input box.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+lightbox.option({
+      'resizeDuration': 200,
+      'wrapAround': true,
+      'alwaysShowNavOnTouchDevices': true,
+      'showImageNumberLabel': false,
+      'maxWidth': 600
+    })
 
+$('.lb-nav').show();
